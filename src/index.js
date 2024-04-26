75705c496de9536a42312355692c3a21f4759bd7
Alexander Skripko <lifeisperfect3@gmail.com>
2024-04-26 17:53:43 +0300

    feat: add javascript code;

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import './style.css';
+import './history/history.css';
+import './map/map.css';
+import './search-button/search-button.css';
+import './search-input/search-input.css';
+import './weather/weather.css';
+import { updateWeatherDom } from './weather/weather';
+import { geoCode, updateMapDom } from './map/map';
+import {
+  updateHistoryBlock,
+  initHistoryBlock,
+  clearHistory,
+} from './history/history';
+import {
+  getSearchString,
+  resetSearchString,
+} from './search-input/search-input';
 
-function run() {
-  console.log('Hello from index.js!');
+async function mainLoop(searchString) {
+  const coords = await geoCode(searchString);
+
+  updateWeatherDom(coords);
+  updateMapDom(coords);
+  updateHistoryBlock(searchString, mainLoop);
 }
 
-run();
+async function buttonHandler() {
+  // Получить название города из текстбокса
+  // Добавить город в localstorage (если есть переполнение, убрать старый)
+  // f()
+  // Удалить текст в текстбоксе
+  // let a = document.getElementsByClassName("search-input")[0];
+  const searchString = getSearchString();
+  mainLoop(searchString);
+  resetSearchString();
+}
+
+// updateHistoryDom();
+initHistoryBlock(mainLoop);
+
+document
+  .getElementsByClassName('history__clear-button')[0]
+  .addEventListener('click', clearHistory);
+
+document
+  .getElementsByClassName('search-button')[0]
+  .addEventListener('click', buttonHandler);
---
+++ src/map/map.js
@@ -0,0 +1,46 @@
+// Получить координаты по поисковому запросу (город, место, адрес и тд).
+// Используется geoCoder от yandex.
+// Используется первый результат в поиске, поэтому поведение при запросе "ул. Ленина" (которая есть в разных городах) не определено.
+// Результат - объект { lat, lon }
+export async function geoCode(place) {
+  const geocoderApiKey = '615fae63-0e13-4511-a439-117f2518ddce';
+  const uri = encodeURI(
+    `https://geocode-maps.yandex.ru/1.x/?apikey=${geocoderApiKey}&geocode=${place}&format=json`,
+  );
+  const resp = await fetch(uri);
+  const res = await resp.json();
+  const [lon, lat] =
+    res.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(
+      ' ',
+    );
+
+  return {
+    lat,
+    lon,
+  };
+}
+
+export async function updateMapDom(coords) {
+  const str2 = await getStaticMap(coords);
+
+  const myNode = document.getElementsByClassName('map')[0];
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
+  }
+  /* document.getElementsByClassName("map")[0].childNodes[0].remove(); */
+  const imgEl = document
+    .getElementsByClassName('map')[0]
+    .appendChild(document.createElement('img'));
+  imgEl.setAttribute('src', str2);
+}
+
+async function getStaticMap(coords) {
+  const yandexStaticMapApiKey = '174d4d04-4e84-434c-b778-edcad039d856';
+  console.log(
+    `https://static-maps.yandex.ru/v1?ll=${coords.lon},${coords.lat}&spn=0.016457,0.00619&apikey=${yandexStaticMapApiKey}`,
+  );
+  const response2 = await fetch(
+    `https://static-maps.yandex.ru/v1?ll=${coords.lon},${coords.lat}&z=11&apikey=${yandexStaticMapApiKey}`,
+  );
+  return response2.url;
+}
